b3acf380f9d6eb04501dd1a769513e3cc846e646
Aaron Davis <aaronzdavis@gmail.com>
2020-12-18 15:26:54 -0800

    add lazy loading for all images

--- pages/index.vue
+++ pages/index.vue
@@ -15,7 +15,7 @@
   <div class="flex-container">
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/american-dream" target="_blank"><img src="~/assets/images/acr009.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/american-dream" target="_blank"><img loading="lazy" src="~/assets/images/acr009.jpg" /></a>
       </div>
       <div class="name">
         ACR009<br />
@@ -26,7 +26,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/a-sound-place" target="_blank"><img src="~/assets/images/acr008.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/a-sound-place" target="_blank"><img loading="lazy" src="~/assets/images/acr008.jpg" /></a>
       </div>
       <div class="name">
         ACR008<br />
@@ -37,7 +37,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/when-we-get-together" target="_blank"><img src="~/assets/images/acr007.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/when-we-get-together" target="_blank"><img loading="lazy" src="~/assets/images/acr007.jpg" /></a>
       </div>
       <div class="name">
         ACR007<br />
@@ -48,7 +48,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/mdf" target="_blank"><img src="~/assets/images/acr006.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/mdf" target="_blank"><img loading="lazy" src="~/assets/images/acr006.jpg" /></a>
       </div>
       <div class="name">
         ACR006<br />
@@ -59,7 +59,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/acid-camp-all-stars-vol-2" target="_blank"><img src="~/assets/images/acr005-15.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/acid-camp-all-stars-vol-2" target="_blank"><img loading="lazy" src="~/assets/images/acr005-15.jpg" /></a>
       </div>
       <div class="name">
         ACR005 1.5<br />
@@ -70,7 +70,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/acid-camp-all-stars-vol-1" target="_blank"><img src="~/assets/images/acr005-10.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/acid-camp-all-stars-vol-1" target="_blank"><img loading="lazy" src="~/assets/images/acr005-10.jpg" /></a>
       </div>
       <div class="name">
         ACR005 1.0<br />
@@ -81,7 +81,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/my-medicine" target="_blank"><img src="~/assets/images/acr004.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/my-medicine" target="_blank"><img loading="lazy" src="~/assets/images/acr004.jpg" /></a>
       </div>
       <div class="name">
         ACR004<br />
@@ -92,7 +92,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/ive-got-acid-on-my-brain" target="_blank"><img src="~/assets/images/acr-iii.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/ive-got-acid-on-my-brain" target="_blank"><img loading="lazy" src="~/assets/images/acr-iii.jpg" /></a>
       </div>
       <div class="name">
         ACR003<br />
@@ -103,7 +103,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/atmosphere-in-5" target="_blank"><img src="~/assets/images/acr-ii.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/atmosphere-in-5" target="_blank"><img loading="lazy" src="~/assets/images/acr-ii.jpg" /></a>
       </div>
       <div class="name">
         ACR002<br />
@@ -114,7 +114,7 @@
     </div>
     <div class="flex-item">
       <div class="cover">
-        <a href="https://acidcamp.bandcamp.com/album/corot-7b" target="_blank"><img src="~/assets/images/acr-i.jpg" /></a>
+        <a href="https://acidcamp.bandcamp.com/album/corot-7b" target="_blank"><img loading="lazy" src="~/assets/images/acr-i.jpg" /></a>
       </div>
       <div class="name">
         ACR001<br />
@@ -134,7 +134,7 @@
   <a class="heading parties-archive-toggle underline">Parties</a>
   <div class="flex-container parties-archive">
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-30.png" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-30.png" /></div>
       <div class="name">
         AC 30<br />
         December 6 2019<br />
@@ -142,7 +142,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-29.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-29.jpg" /></div>
       <div class="name">
         AC 29<br />
         July 4 2019<br />
@@ -150,7 +150,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-28.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-28.jpg" /></div>
       <div class="name">
         AC 28<br />
         May 11 2019<br />
@@ -158,7 +158,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-27.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-27.jpg" /></div>
       <div class="name">
         AC 27<br />
         April 13 2019<br />
@@ -166,7 +166,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-26.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-26.jpg" /></div>
       <div class="name">
         AC 26<br />
         June 17 2018<br />
@@ -174,7 +174,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-25.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-25.jpg" /></div>
       <div class="name">
         AC 25<br />
         May 20 2018<br />
@@ -182,7 +182,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-24.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-24.jpg" /></div>
       <div class="name">
         AC 24<br />
         March 11 2018<br />
@@ -190,7 +190,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-23.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-23.jpg" /></div>
       <div class="name">
         AC 23<br />
         Feb 3 2018<br />
@@ -198,7 +198,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xxii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xxii.jpg" /></div>
       <div class="name">
         AC 22<br />
         July 1 2017<br />
@@ -206,7 +206,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xxi_sq.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xxi_sq.jpg" /></div>
       <div class="name">
         AC 21<br />
         April 9 2017<br />
@@ -214,7 +214,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xx.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xx.jpg" /></div>
       <div class="name">
         AC 20<br />
         Feb 25 2017<br />
@@ -222,7 +222,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xix_sq.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xix_sq.jpg" /></div>
       <div class="name">
         AC 19<br />
         Feb 9 + 11 2017<br />
@@ -230,7 +230,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xviii_sq.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xviii_sq.jpg" /></div>
       <div class="name">
         AC 18<br />
         Jan 28 2017<br />
@@ -238,7 +238,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xvii_sq.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xvii_sq.jpg" /></div>
       <div class="name">
         AC 17<br />
         Jan 13 2017<br />
@@ -246,7 +246,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xvi.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xvi.jpg" /></div>
       <div class="name">
         AC 16<br />
         Nov 06 2016<br />
@@ -254,7 +254,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xv.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xv.jpg" /></div>
       <div class="name">
         AC 15<br />
         August 14 2016<br />
@@ -262,7 +262,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xiv.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xiv.jpg" /></div>
       <div class="name">
         AC 14<br />
         July 30 2016<br />
@@ -270,7 +270,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xiii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xiii.jpg" /></div>
       <div class="name">
         AC 13<br />
         June 24 2016<br />
@@ -278,7 +278,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xii.jpg" /></div>
       <div class="name">
         AC 12<br />
         May 22 2016<br />
@@ -286,7 +286,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-xi.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-xi.jpg" /></div>
       <div class="name">
         AC 11<br />
         May 14 2016<br />
@@ -294,7 +294,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-x.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-x.jpg" /></div>
       <div class="name">
         AC 10<br />
         March 27 2016<br />
@@ -302,7 +302,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-ix.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-ix.jpg" /></div>
       <div class="name">
         AC 9<br />
         March 5 2016<br />
@@ -310,7 +310,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-viii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-viii.jpg" /></div>
       <div class="name">
         AC 8<br />
         Feb 28 2016<br />
@@ -318,7 +318,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-vii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-vii.jpg" /></div>
       <div class="name">
         AC 7<br />
         Jan 24 2016<br />
@@ -326,7 +326,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-vi.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-vi.jpg" /></div>
       <div class="name">
         AC 6<br />
         Dec 20 2015<br />
@@ -334,7 +334,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-v.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-v.jpg" /></div>
       <div class="name">
         AC 5<br />
         October 25 2015<br />
@@ -342,7 +342,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-iv.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-iv.jpg" /></div>
       <div class="name">
         AC 4<br />
         Aug 23 2015<br />
@@ -350,7 +350,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-iii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-iii.jpg" /></div>
       <div class="name">
         AC 3<br />
         July 12 2015<br />
@@ -358,7 +358,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-ii.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-ii.jpg" /></div>
       <div class="name">
         AC 2<br />
         June 14 2015<br />
@@ -366,7 +366,7 @@
       </div>
     </div>
     <div class="flex-item">
-      <div class="cover"><img src="~assets/images/ac-i.jpg" /></div>
+      <div class="cover"><img loading="lazy" src="~assets/images/ac-i.jpg" /></div>
       <div class="name">
         AC 1<br />
         May 10 2015<br />
